84bf884d49514e9d1297adac698b7f103de74623
Andre Felippe <afelipp3@gmail.com>
2021-08-03 17:10:34 -0300

    Create Loader component

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+  return (
+    <>
+      <span className="animate-loader ease-linear rounded-full border-t-2 border-blue-500 h-12 w-12  mx-auto"></span>
+      <p className="text-center text-black text-xl font-semibold">Loading...</p>
+    </>
+  )
+}

25daf5df4cf088cda12ec3ab1cce5ecef6966950
Andre Felippe <afelipp3@gmail.com>
2021-08-03 17:13:27 -0300

    Create custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,44 @@
 module.exports = {
+  mode: 'jit',
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {
-      backgroundColor: ['even'],
+      animation: {
+        'scale-up': 'scaleUp 1s forwards',
+        loader: 'spinner 1.5s linear infinite',
+      },
+      keyframes: {
+        scaleUp: {
+          from: {
+            opacity: '0',
+            transform: 'scale(0.8)',
+          },
+          to: {
+            opacity: 'initial',
+            transform: 'initial',
+          },
+        },
+
+        spinner: {
+          '0%': {
+            transform: 'rotate(0deg)',
+          },
+          '100%': {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
     },
+
+    variants: {
+      extend: {
+        backgroundColor: ['even'],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }

0f6d7765607b462395eaa6cf1fe6b002e71a00e5
Andre Felippe <afelipp3@gmail.com>
2021-08-03 17:11:54 -0300

    Add open modal animation

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -14,7 +14,7 @@ export const Modal = ({ showModal, closeModal, patient }: ModalProps) => {
 
   return (
     <div className="modal fixed inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50">
-      <div className="modal-content w-3/4 md:w-auto bg-white text-sm md:text-base xl:text-xl text-center py-8 px:8 md:px-20 rounded-md">
+      <div className="animate-scale-up modal-content w-3/4 md:w-auto bg-white text-sm md:text-base xl:text-xl text-center py-8 px:8 md:px-20 rounded-md">
         <div className="modal-header my-0 mx-auto md:max-w-2xl">
           <Image
             className="rounded-full relative bottom-20"
@@ -22,6 +22,8 @@ export const Modal = ({ showModal, closeModal, patient }: ModalProps) => {
             alt={patient.name.first}
             height={162}
             width={162}
+            placeholder="blur"
+            blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
           />
           <h1 className="text-center my-5 font-semibold">{`${patient.name.first} ${patient.name.last}`}</h1>
           <h2 className="font-medium">Id: {patient.login.uuid}</h2>
